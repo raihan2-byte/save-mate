ab1a82e5d40def4f1c90dd27c8e36d4ea41b3316
raihan2-byte <raihanalfarisi4@gmail.com>
2026-02-28 03:37:08 +0700

    create query for repository personal data and join with user

--- repository/personal_data_repository.go
+++ repository/personal_data_repository.go
@@ -9,6 +9,7 @@ type PersonalDataRepository interface {
 	Create(personalData *personaldata.PersonalData) (*personaldata.PersonalData, error)
 	FindByPersonalDataId(personalDataId string) (*personaldata.PersonalData, error)
 	FindPersonalDataByUserId(userId string) (*personaldata.PersonalData, error)
+	FindAllPersonalData(limit int, offset int) ([]*personaldata.PersonalData, error)
 }
 
 type personalDataRepository struct {
@@ -30,5 +31,66 @@ func (r *personalDataRepository) Create(personalData *personaldata.PersonalData)
 	idPersonalData, _ := result.LastInsertId()
 	personalData.Id = int(idPersonalData)
 
+	return personalData, nil
+}
+
+func (r *personalDataRepository) FindPersonalDataByUserId(userId string) (*personaldata.PersonalData, error) {
+	query := "SELECT pd.personal_data_id, pd.user_id, pd.sallary, pd.purpose_of_join_here, pd.saving_type, pd.created_at, pd.updated_at, u.user_id, u.username u.email from personal_datas pd JOIN users u ON u.user_id = pd.user_id WHERE pd.user_id = ? "
+
+	row := r.db.QueryRow(query, userId)
+
+	personalData := &personaldata.PersonalData{}
+	err := row.Scan(&personalData.PersonalDataId, &personalData.UserId, &personalData.Sallary,
+		&personalData.PurposeOfJoinHere, &personalData.SavingType, &personalData.CreatedAt, personalData.UpdatedAt, &personalData.User.UserId, &personalData.User.Username,
+		&personalData.User.Email)
+
+	if err != nil {
+		return personalData, err
+	}
+	return personalData, nil
+}
+
+func (r *personalDataRepository) FindAllPersonalData(limit int, offset int) ([]*personaldata.PersonalData, error) {
+	query := "SELECT pd.personal_data_id, pd.user_id, pd.sallary, pd.purpose_of_join_here, pd.saving_type, pd.created_at, pd.updated_at, u.user_id, u.username, u.email from personal_datas pd JOIN users u ON  u.user_id = pd.user_id ORDER BY pd.created_at DESC LIMIT ? OFFSET ?"
+
+	row, err := r.db.Query(query, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	var personalDatas []*personaldata.PersonalData
+
+	for row.Next() {
+
+		pd := &personaldata.PersonalData{}
+		err := row.Scan(&pd.PersonalDataId, &pd.UserId, &pd.Sallary,
+			&pd.PurposeOfJoinHere, &pd.SavingType, &pd.CreatedAt, pd.UpdatedAt, &pd.User.UserId, &pd.User.Username,
+			&pd.User.Email)
+
+		if err != nil {
+			return nil, err
+		}
+		personalDatas = append(personalDatas, pd)
+	}
+	if err = row.Err(); err != nil {
+		return nil, err
+	}
+
+	return personalDatas, nil
+}
+
+func (r *personalDataRepository) FindByPersonalDataId(personalDataId string) (*personaldata.PersonalData, error) {
+	query := "SELECT pd.personal_data_id, pd.user_id, pd.sallary, pd.purpose_of_join_here, pd.saving_type, pd.created_at, pd.updated_at, u.user_id, u.username, u.email from personal_datas pd JOIN user u ON pd.user_id = u.user_id ORDER BY pd.created_at DESC WHERE personal_data_id = ?"
+
+	row := r.db.QueryRow(query, personalDataId)
+
+	personalData := &personaldata.PersonalData{}
+	err := row.Scan(&personalData.PersonalDataId, &personalData.UserId, &personalData.Sallary,
+		&personalData.PurposeOfJoinHere, &personalData.SavingType, &personalData.CreatedAt, personalData.UpdatedAt, &personalData.User.UserId, &personalData.User.Username,
+		&personalData.User.Email)
+
+	if err != nil {
+		return personalData, err
+	}
 	return personalData, nil
 }
